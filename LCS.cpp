9d18302f533a0a25b06795fcbc4b5c29b39d0a17
Chaitanya-modi <chaitanyamodi711@gmail.com>
2024-04-28 19:25:33 +0530

    hopefully fixed input thingy

--- LCS.cpp
+++ LCS.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "lcs.h"
 
 using namespace std;
+
 // C is the LCS number matrix and B is direction matrix, he used arrows, I'm using D,U,L
 void LCS_Length(const char *X, const char *Y, int m, int n, char B[51][51], int C[51][51])
 {
@@ -75,48 +77,4 @@ char *ExtractLCS(const char *X, char B[51][51], int m, int n)
     lcs[index] = '\0';
     Reverse_String(lcs, index); // reverse the string because it was constructed backwards
     return lcs;
-}
-
-int main()
-{
-    const int m = 22;
-    const int n = 17;
-    char X[m + 1] = "IDON'TWANTTOKILLMYSELF";
-    char Y[n + 1] = "IWANTTOKILLMYSELF";
-    int C[51][51] = {0};
-    char B[51][51] = {0};
-    char *lcs;
-
-    cout << "String 1: " << X << endl;
-    cout << "String 2: " << Y << endl;
-
-    LCS_Length(X, Y, m, n, B, C);
-    cout << "Length of LCS: " << C[m][n] << endl;
-
-    cout << "C matrix:" << endl;
-    for (int i = 0; i <= m; i++)
-    {
-        for (int j = 0; j <= n; j++)
-        {
-            printf("%4d", C[i][j]);
-        }
-        cout << endl;
-    }
-
-    cout << "B matrix:" << endl;
-    for (int i = 1; i <= m; i++) // starting from 1 because 0th row and column are 0s
-    {
-        for (int j = 1; j <= n; j++)
-        {
-            printf("%4c", B[i][j]);
-        }
-        cout << endl;
-    }
-
-    lcs = ExtractLCS(X, B, m, n);
-    cout << "LCS: " << lcs << endl;
-
-    delete[] lcs; // free allocated memory
-
-    return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "lcs.h" 
+#include "lcs.h"
+#include "lcs.cpp"
 
 using namespace std;
 
-int main() {
-    ifstream input_file("./input/input4.txt"); // Open the input file
-    if (!input_file.is_open()) {
+int main()
+{
+    ifstream input_file("./input/input6.txt"); // Open the input file
+    if (!input_file.is_open())
+    {
         cout << "File does not exist" << endl;
         return 1;
     }
@@ -15,13 +18,13 @@ int main() {
     string X, Y;
     getline(input_file, X); // Read the first string from the file
     getline(input_file, Y); // Read the second string from the file
-    input_file.close(); // Close the input file
+    input_file.close();     // Close the input file
 
     const int m = X.length();
     const int n = Y.length();
     char B[51][51];
     int C[51][51];
-    char lcs[max(m, n) + 1];
+    char *lcs;
 
     cout << "String 1: " << X << endl;
     cout << "String 2: " << Y << endl;
@@ -31,24 +34,29 @@ int main() {
     cout << endl;
 
     cout << "C matrix:" << endl;
-    for (int i = 0; i <= m; i++) {
-        for (int j = 0; j <= n; j++) {
-            cout << C[i][j] << " ";
+    for (int i = 0; i <= m; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            printf("%4d", C[i][j]);
         }
         cout << endl;
     }
 
-    cout << endl;
     cout << "B matrix:" << endl;
-    for (int i = 0; i <= m; i++) {
-        for (int j = 0; j <= n; j++) {
-            cout << B[i][j] << " ";
+    for (int i = 1; i <= m; i++) // starting from 1 because 0th row and column are 0s
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            printf("%4c", B[i][j]);
         }
         cout << endl;
     }
 
-    ExtractLCS(X.c_str(), B, m, n, lcs);
+    lcs = ExtractLCS(X.c_str(), B, m, n);
     cout << "LCS: " << lcs << endl;
+
+    delete[] lcs; // free allocated memory
 
     return 0;
 }
