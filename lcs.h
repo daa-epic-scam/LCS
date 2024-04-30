2ecda216b799e0a7f4aaf0350cd6dcd0d94c6a8d
Mukundan Gurumurthy <kailashguru129@gmail.com>
2024-04-30 12:01:39 +0530

    fix

--- lcs.h
+++ lcs.h
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void LCS_Length(const string &X, const char *Y, int m, int n, char **B, int **C);
+void LCS_Length(const string &X, const string &Y, int m, int n, char **B, int **C);
 void Reverse_String(string &str, int length);
 string ExtractLCS(const string &X, char **B, int m, int n);
 
